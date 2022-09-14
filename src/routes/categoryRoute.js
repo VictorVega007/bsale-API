0e5d9ed54e60947a8f172cf32b83c07188878480
victorv <victorvega.v@gmail.com>
2022-09-13 21:02:16 -0300

    Adding controller category

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -3,9 +3,7 @@
 const { Router } = require('express');
 const router = Router();
 const { verifyingCache } = require('../middleware/appCacheMiddleware');
-// const CategoryOfProducts = require('../models/productsCategory');
 const CategoryController = require('../controllers/category_controller');
-// const CategoryOfProducts = require('../models/productsCategory');
 
 /**
  * @swagger
@@ -40,20 +38,8 @@ const CategoryController = require('../controllers/category_controller');
  *                   $ref: '#/components/schemas/Categories'
  */
 
-// router.get('/', verifyingCache, async (req, res) => {
-//     // const productsCategory = await CategoryOfProducts.findAll();
-//     const productsCategory = CategoryController.index()
-//     console.log(productsCategory)
-//     const setCache = appCache.set(req.originalUrl, productsCategory);
-
-//     setCache 
-//         ? res.status(200).send(productsCategory) 
-//         : res.status(404).json({error: `Category of ${productsCategory} not found`});
-// });
 
 router.route('/', verifyingCache)
     .get(CategoryController.index)
-
-
 
 module.exports = router;
